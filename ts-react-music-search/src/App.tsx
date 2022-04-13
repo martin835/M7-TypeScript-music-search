3fd93d36d39a6d17ce284cbba7844a8c3d66b25d
martin835 <64438132+martin835@users.noreply.github.com>
2022-04-13 14:38:48 +0200

    initial set-up done, bootstraped installed and hooked

---
+++ ts-react-music-search/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Jumbotron, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
